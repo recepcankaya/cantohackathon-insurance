cfe3d584abe6ae95c191d9769b3257cab8a44d50
Recep Cankaya <recepcnky@outlook.com>
2023-03-31 19:49:49 +0300

    Add sendContract function

--- canto-frontend/components/NavBar.jsx
+++ canto-frontend/components/NavBar.jsx
@@ -1,12 +1,42 @@
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext } from "react";
+import { startTransition, useContext } from "react";
 import { ContextAPI } from "../context/ContextProvider";
+import {
+  MANAGEMENT_CONTRACT_ADDRESS,
+  INSURANCE_CONTRACT_ADDRESS,
+} from "../constants";
 import styles from "@/styles/NavBar.module.css";
+import { providers, utils } from "ethers";
 
 export default function NavBar() {
-  const { connectWallet, walletConnected, address } = useContext(ContextAPI);
+  const {
+    connectWallet,
+    walletConnected,
+    address,
+    insuranceContractInstance,
+    getProviderOrSigner,
+  } = useContext(ContextAPI);
   const router = useRouter();
+
+  const insuranceBalance = async () => {
+    const provider = await getProviderOrSigner();
+    const balance = provider.getBalance(INSURANCE_CONTRACT_ADDRESS);
+    return balance;
+  };
+
+  const sendTokensToContract = async () => {
+    const signer = await getProviderOrSigner(true);
+    const contract = await insuranceContractInstance(signer);
+    const balance = await insuranceBalance();
+    const str = (Number(balance) / 1e18).toString();
+    const send = await contract.withdrawTokensToContract(
+      MANAGEMENT_CONTRACT_ADDRESS,
+      {
+        value: utils.parseEther(str),
+      }
+    );
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -29,6 +59,9 @@ export default function NavBar() {
           Pay The Base Fee
         </Link>
       </div>
+      <button onClick={sendTokensToContract} className={styles.send}>
+        Send To Contract
+      </button>
       <button className={styles.clientWallet} onClick={connectWallet}>
         {walletConnected ? "Wallet Connected" : "Connect Wallet"}: {""}
         {address.slice(0, 5)}...{address.slice(-4)}
